08b25b06af737079604e8893d0b9dd830fb6d3f3
vishal <vishalsainani2000@gmail.com>
2023-07-11 12:31:50 +0500

    chat app complete

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
+require('dotenv').config({ path: '.env.local' });
 
 const app = express();
 app.use(express.json());
@@ -7,7 +9,18 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
     const { username } = req.body;
-    return res.json({ username: username, secret: "sha256..." });
+    // Get or create user on Chat Engine!
+    try {
+        const r = await axios.put(
+            "https://api.chatengine.io/users/",
+            { username: username, secret: username, first_name: username },
+            { headers: { "Private-Key": process.env.PRIVATE_KEY } }
+
+        );
+        return res.status(r.status).json(r.data);
+    } catch (e) {
+        return res.status(e.response.status).json(e.response.data);
+    }
 });
 
 app.listen(3001);
